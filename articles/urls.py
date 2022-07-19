cd1c06ea5c644fa7b44b9ce06469d2a96ab0ae07
Alexander Waya Nguku <alex.waya@gmail.com>
2022-07-19 16:17:51 +0300

    config the articles app

---
+++ articles/urls.py
@@ -0,0 +1,8 @@
+#from django.views.generic import ListView, DetailView
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ArticleListView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='detail'),
+]
